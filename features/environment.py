84b560cbf78f3fe9c46c86c3c91ef1ea96550f74
marek6 <marek.partyka@britenet.com.pl>
2020-09-28 14:57:33 +0200

    Save screenshot after failed scenario

--- features/environment.py
+++ features/environment.py
@@ -1,3 +1,5 @@
+import os
+
 from features.base.webdriver_factory import WebDriverFactory
 from features.managers.page_manager import PageManager
 
@@ -11,4 +13,11 @@ def before_scenario(context, scenario):
 
 
 def after_scenario(context, scenario):
+    print("scenario status: " + scenario.status.name)
+    if scenario.status.name == "failed":
+        if not os.path.exists("failed_scenarios_screenshots"):
+            os.makedirs("failed_scenarios_screenshots")
+        os.chdir("failed_scenarios_screenshots")
+        context.browser.save_screenshot(scenario.name + "_failed.png")
+
     context.browser.quit()
